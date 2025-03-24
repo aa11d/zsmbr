6c03be5766b5b232b5a83d9d914867694db8e544
Pásztor Zsombor <pasztor.zsombor@blathy.info>
2025-03-24 09:37:44 +0100

    Rendezesek - buborek

---
+++ szargepesorai_03_24/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Rendezesek 
+{
+
+    static int[] tomb;
+    const int N = 20;
+
+    static void kiir()
+    {
+        for(int i=0; i<N; i++)
+            System.Console.Write(tomb[i]+" ");
+        System.Console.WriteLine();
+    }
+
+    static void csere(int i, int j)
+    {
+        int plusz = tomb[i];
+        tomb[i] = tomb[j];
+        tomb[j] = plusz;
+    }
+
+    static void buborek()
+    {
+        for(int meddig = N-2; meddig >=0; meddig--)
+        {
+            for(int mutato=0; mutato<=meddig; mutato++)
+            {
+                if(tomb[mutato] > tomb[mutato+1])
+                    csere(mutato, mutato+1);
+            }
+        }
+    }
+
+    static void Main()
+    {
+
+        tomb = new int[N];
+        Random rnd = new ();
+        for(int i = 0; i<N; i++)
+            tomb[i] = rnd.Next(10, 100);
+        kiir();
+        buborek();
+        kiir();
+    }
+}
